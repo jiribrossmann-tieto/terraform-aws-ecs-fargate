0a0e328cb083062618d0f91a4315fdc189cde4b0
Jiri Brossmann <jiri.brossmann@teliacompany.com>
2020-01-21 12:25:05 +0100

    Add permissions ssm:GetParameters

--- policies.tf
+++ policies.tf
@@ -43,6 +43,7 @@ data "aws_iam_policy_document" "task_execution_permissions" {
       "ecr:BatchGetImage",
       "logs:CreateLogStream",
       "logs:PutLogEvents",
+      "ssm:GetParameters",
     ]
   }
 }
